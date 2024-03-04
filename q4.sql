075f7ee989b669b99f926e5a7e64ea52f538b419
avramid9 <andreas.avramidis@mail.utoronto.ca>
2024-03-04 18:40:35 -0500

    Q4 solution

--- q4.sql
+++ q4.sql
@@ -12,10 +12,40 @@ CREATE TABLE q4 (
 -- (But give them better names!) The IF EXISTS avoids generating an error
 -- the first time this file is imported.
 -- If you do not define any views, you can delete the lines about views.
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS EventLibrary CASCADE;
+DROP VIEW IF EXISTS EventWard CASCADE;
+DROP VIEW IF EXISTS PatronEventDetails CASCADE;
+DROP VIEW IF EXISTS AllWards CASCADE;
 
 -- Define views for your intermediate steps here:
-CREATE VIEW intermediate_step AS ... ;
+-- Get each event id with the corresponding library it is in
+CREATE VIEW EventLibrary AS
+SELECT le.id, lr.library
+FROM LibraryEvent le JOIN LibraryRoom lr ON le.room = lr.id;
+
+-- Get each event with the corresponding ward it is in
+CREATE VIEW EventWard AS
+SELECT el.id, lb.ward
+FROM EventLibrary el JOIN LibraryBranch lb ON el.library = lb.code;
+
+-- For each event a patron signs up for have the ward it takes place in as well as the date
+CREATE VIEW PatronEventDetails AS
+SELECT esu.patron, esu.event, ew.ward, EXTRACT(YEAR FROM es.edate) AS year
+FROM EventSignUp esu
+JOIN EventWard ew ON esu.event = ew.id
+JOIN EventSchedule es ON ew.id = es.event;
+
+-- GET each patron who in each year signed up for an event in each ward
+CREATE VIEW AllWards AS
+SELECT patron, year
+FROM PatronEventDetails
+GROUP BY patron, year
+HAVING count(DISTINCT ward) = (
+    SELECT count(id)
+    FROM Ward
+);
 
 -- Your query that answers the question goes below the "insert into" line:
 INSERT INTO q4
+SELECT DISTINCT patron AS patronID
+FROM AllWards;
